32e2cff00fc367c17d2e7617875ad98474c0b33e
romakv4 <romakv4@gmail.com>
2019-07-11 19:33:17 +0600

    periodic incomes dto requireded fields

--- cost_income_calculator.api/Dtos/IncomeDtos/PeriodicIncomesDto.cs
+++ cost_income_calculator.api/Dtos/IncomeDtos/PeriodicIncomesDto.cs
@@ -1,10 +1,13 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace cost_income_calculator.api.Dtos.IncomeDtos
 {
     public class PeriodicIncomesDto
     {
+        [Required]
         public string Username { get; set; }
+        [Required]
         public DateTime Date { get; set; }
     }
 }

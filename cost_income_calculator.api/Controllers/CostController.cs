8d43aa4fb2aa5d463b23c33230c853bde1644ac8
romakv4 <romakv4@gmail.com>
2019-07-12 07:10:57 +0600

    naive errors catching for client

--- cost_income_calculator.api/Controllers/CostController.cs
+++ cost_income_calculator.api/Controllers/CostController.cs
@@ -27,100 +27,162 @@ namespace cost_income_calculator.api.Controllers
         [HttpPost]
         public async Task<IActionResult> GetAllCosts(CostForGetDto costForGetDto)
         {
-            if (!await userHelper.UserExists(costForGetDto.Username))
-                return BadRequest("This username doesn't exists");
+            try
+            {
+                if (!await userHelper.UserExists(costForGetDto.Username))
+                    return BadRequest("This username doesn't exists");
             
-            var costs = await repository.GetAllCosts(costForGetDto);
-            
-            return Ok(costs);
+                var costs = await repository.GetAllCosts(costForGetDto);
+                return Ok(costs);
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
         }
 
         [HttpPost("weekly")]
         public async Task<IActionResult> GetWeeklyCosts(PeriodicCostsDto periodicCostsDto)
         {
-            if (!await userHelper.UserExists(periodicCostsDto.Username))
-                return BadRequest("This username doesn't exists");
-            
-            var weeklyCosts = await repository.GetWeeklyCosts(periodicCostsDto);
-            
-            return Ok(weeklyCosts);
+            try
+            {
+                if (!await userHelper.UserExists(periodicCostsDto.Username))
+                    return BadRequest("This username doesn't exists");
+                
+                var weeklyCosts = await repository.GetWeeklyCosts(periodicCostsDto);
+                
+                return Ok(weeklyCosts);
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
         }
 
         [HttpPost("weekly/{category}")]
         public async Task<IActionResult> GetWeeklyCostsByCategory(PeriodicCostsDto periodicCostsDto, string category)
         {
-            if (!await userHelper.UserExists(periodicCostsDto.Username))
-                return BadRequest("This username doesn't exists");
+            try
+            {
+                if (!await userHelper.UserExists(periodicCostsDto.Username))
+                    return BadRequest("This username doesn't exists");
             
-            var weeklyCostsByCategory = await repository.GetWeeklyCostsByCategory(periodicCostsDto, category);
-            
-            return Ok(weeklyCostsByCategory);
+                var weeklyCostsByCategory = await repository.GetWeeklyCostsByCategory(periodicCostsDto, category);
+                
+                return Ok(weeklyCostsByCategory);
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
         }
 
         [HttpPost("monthly")]
         public async Task<IActionResult> GetMonthlyCosts(PeriodicCostsDto periodicCostsDto)
         {
-            if (!await userHelper.UserExists(periodicCostsDto.Username))
-                return BadRequest("This username doesn't exists");
+            try
+            {
+                if (!await userHelper.UserExists(periodicCostsDto.Username))
+                    return BadRequest("This username doesn't exists");
             
-            var monthlyCosts = await repository.GetMonthlyCosts(periodicCostsDto);
-            
-            return Ok(monthlyCosts);
+                var monthlyCosts = await repository.GetMonthlyCosts(periodicCostsDto);
+                
+                return Ok(monthlyCosts);
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
         }
 
         [HttpPost("monthly/{category}")]
         public async Task<IActionResult> GetMonthlyCostsByCategory(PeriodicCostsDto periodicCostsDto, string category)
         {
-            if (!await userHelper.UserExists(periodicCostsDto.Username))
-                return BadRequest("This username doesn't exists");
+            try
+            {
+                if (!await userHelper.UserExists(periodicCostsDto.Username))
+                    return BadRequest("This username doesn't exists");
             
-            var monthlyCostsByCategory = await repository.GetMonthlyCostsByCategory(periodicCostsDto, category);
-            
-            return Ok(monthlyCostsByCategory);
+                var monthlyCostsByCategory = await repository.GetMonthlyCostsByCategory(periodicCostsDto, category);
+                
+                return Ok(monthlyCostsByCategory);
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
         }
 
         [HttpPost("monthly/max")]
         public async Task<IActionResult> GetMaxMonthlyCosts(PeriodicCostsDto periodicCostsDto)
         {
-            if (!await userHelper.UserExists(periodicCostsDto.Username))
-                return BadRequest("This username doesn't exists");
+            try
+            {
+                if (!await userHelper.UserExists(periodicCostsDto.Username))
+                    return BadRequest("This username doesn't exists");
             
-            var maxMonthlyCosts = await repository.GetMaxCostsCategoryInMonth(periodicCostsDto);
-            
-            return Ok(maxMonthlyCosts);
+                var maxMonthlyCosts = await repository.GetMaxCostsCategoryInMonth(periodicCostsDto);
+                
+                return Ok(maxMonthlyCosts);
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
         }
 
         [HttpPost("set")]
         public async Task<IActionResult> SetCost(CostForSetDto costForSetDto)
         {
-            if (!await userHelper.UserExists(costForSetDto.Username))
-                return BadRequest("This username doesn't exists");
+            try
+            {
+                if (!await userHelper.UserExists(costForSetDto.Username))
+                    return BadRequest("This username doesn't exists");
 
-            var settedCost = await repository.SetCost(costForSetDto);
-            
-            return StatusCode(201);
+                var settedCost = await repository.SetCost(costForSetDto);
+                
+                return StatusCode(201);
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
         }
 
         [HttpPut("edit/{id}")]
         public async Task<IActionResult> EditCost(int id, CostForEditDto costForEditDto)
         {
-            if (!await userHelper.UserExists(costForEditDto.Username))
-                return BadRequest("This username doesn't exists");
+            try
+            {
+                if (!await userHelper.UserExists(costForEditDto.Username))
+                    return BadRequest("This username doesn't exists");
             
-            var editedCost = await repository.EditCost(id, costForEditDto);
+                var editedCost = await repository.EditCost(id, costForEditDto);
 
-            return StatusCode(204);
+                return StatusCode(204);
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
         }
 
         [HttpDelete("delete")]
         public async Task<IActionResult> DeleteCosts(CostForDeleteDto costForDeleteDto)
         {
-            if (!await userHelper.UserExists(costForDeleteDto.Username))
-                return BadRequest("This username doesn't exists");
+            try
+            {
+                if (!await userHelper.UserExists(costForDeleteDto.Username))
+                    return BadRequest("This username doesn't exists");
 
-            var deletedCosts = await repository.DeleteCosts(costForDeleteDto);
+                var deletedCosts = await repository.DeleteCosts(costForDeleteDto);
 
-            return StatusCode(204);
+                return StatusCode(204);
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
         }
     }
 }
--- cost_income_calculator.api/Controllers/IncomeController.cs
+++ cost_income_calculator.api/Controllers/IncomeController.cs
@@ -26,100 +26,163 @@ namespace cost_income_calculator.api.Controllers
         [HttpPost]
         public async Task<IActionResult> GetAllIncomes(IncomeForGetDto incomeForGetDto)
         {
-            if (!await userHelper.UserExists(incomeForGetDto.Username))
-                return BadRequest("This username doesn't exists");
-            
-            var incomes = await repository.GetAllIncomes(incomeForGetDto);
-            
-            return Ok(incomes);
+            try
+            {
+                if (!await userHelper.UserExists(incomeForGetDto.Username))
+                    return BadRequest("This username doesn't exists");
+                
+                var incomes = await repository.GetAllIncomes(incomeForGetDto);
+                
+                return Ok(incomes);
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
         }
 
         [HttpPost("weekly")]
         public async Task<IActionResult> GetWeeklyIncomes(PeriodicIncomesDto periodicIncomesDto)
         {
-            if (!await userHelper.UserExists(periodicIncomesDto.Username))
-                return BadRequest("This username doesn't exists");
-            
-            var weeklyIncomes = await repository.GetWeeklyIncomes(periodicIncomesDto);
-            
-            return Ok(weeklyIncomes);
+            try
+            {
+                if (!await userHelper.UserExists(periodicIncomesDto.Username))
+                    return BadRequest("This username doesn't exists");
+                
+                var weeklyIncomes = await repository.GetWeeklyIncomes(periodicIncomesDto);
+                
+                return Ok(weeklyIncomes);
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
         }
 
         [HttpPost("weekly/{category}")]
         public async Task<IActionResult> GetWeeklyIncomesByCategory(PeriodicIncomesDto periodicIncomesDto, string category)
         {
-            if (!await userHelper.UserExists(periodicIncomesDto.Username))
-                return BadRequest("This username doesn't exists");
-            
-            var weeklyIncomesByCategory = await repository.GetWeeklyIncomesByCategory(periodicIncomesDto, category);
-            
-            return Ok(weeklyIncomesByCategory);
+            try
+            {
+                if (!await userHelper.UserExists(periodicIncomesDto.Username))
+                    return BadRequest("This username doesn't exists");
+                
+                var weeklyIncomesByCategory = await repository.GetWeeklyIncomesByCategory(periodicIncomesDto, category);
+                
+                return Ok(weeklyIncomesByCategory);
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
         }
 
         [HttpPost("monthly")]
         public async Task<IActionResult> GetMonthlyIncomes(PeriodicIncomesDto periodicIncomesDto)
         {
-            if (!await userHelper.UserExists(periodicIncomesDto.Username))
-                return BadRequest("This username doesn't exists");
-            
-            var monthlyIncomes = await repository.GetMonthlyIncomes(periodicIncomesDto);
-            
-            return Ok(monthlyIncomes);
+            try
+            {
+                if (!await userHelper.UserExists(periodicIncomesDto.Username))
+                    return BadRequest("This username doesn't exists");
+                
+                var monthlyIncomes = await repository.GetMonthlyIncomes(periodicIncomesDto);
+                
+                return Ok(monthlyIncomes);
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
         }
 
         [HttpPost("monthly/{category}")]
         public async Task<IActionResult> GetMonthlyIncomesByCategory(PeriodicIncomesDto periodicIncomesDto, string category)
         {
-            if (!await userHelper.UserExists(periodicIncomesDto.Username))
-                return BadRequest("This username doesn't exists");
-            
-            var monthlyIncomesByCategory = await repository.GetMonthlyIncomesByCategory(periodicIncomesDto, category);
-            
-            return Ok(monthlyIncomesByCategory);
+            try
+            {
+                if (!await userHelper.UserExists(periodicIncomesDto.Username))
+                    return BadRequest("This username doesn't exists");
+                
+                var monthlyIncomesByCategory = await repository.GetMonthlyIncomesByCategory(periodicIncomesDto, category);
+                
+                return Ok(monthlyIncomesByCategory);
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
         }
 
         [HttpPost("monthly/max")]
         public async Task<IActionResult> GetMaxMonthlyIncomes(PeriodicIncomesDto periodicIncomesDto)
         {
-            if (!await userHelper.UserExists(periodicIncomesDto.Username))
-                return BadRequest("This username doesn't exists");
-            
-            var maxMonthlyIncomes = await repository.GetMaxIncomesCategoryInMonth(periodicIncomesDto);
-            
-            return Ok(maxMonthlyIncomes);
+            try
+            {
+                if (!await userHelper.UserExists(periodicIncomesDto.Username))
+                    return BadRequest("This username doesn't exists");
+                
+                var maxMonthlyIncomes = await repository.GetMaxIncomesCategoryInMonth(periodicIncomesDto);
+                
+                return Ok(maxMonthlyIncomes);
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
         }
 
         [HttpPost("set")]
         public async Task<IActionResult> SetIncome(IncomeForSetDto incomeForSetDto)
         {
-            if (!await userHelper.UserExists(incomeForSetDto.Username))
-                return BadRequest("This username doesn't exists");
+            try
+            {
+                if (!await userHelper.UserExists(incomeForSetDto.Username))
+                    return BadRequest("This username doesn't exists");
 
-            var settedIncome = await repository.SetIncome(incomeForSetDto);
-            
-            return StatusCode(201);
+                var settedIncome = await repository.SetIncome(incomeForSetDto);
+                
+                return StatusCode(201);
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
         }
 
         [HttpPut("edit/{id}")]
         public async Task<IActionResult> EditIncome(int id, IncomeForEditDto incomeForEditDto)
         {
-            if (!await userHelper.UserExists(incomeForEditDto.Username))
-                return BadRequest("This username doesn't exists");
-            
-            var editedIncome = await repository.EditIncome(id, incomeForEditDto);
+            try
+            {
+                if (!await userHelper.UserExists(incomeForEditDto.Username))
+                    return BadRequest("This username doesn't exists");
+                
+                var editedIncome = await repository.EditIncome(id, incomeForEditDto);
 
-            return StatusCode(204);
+                return StatusCode(204);
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
         }
 
         [HttpDelete("delete")]
         public async Task<IActionResult> DeleteIncomes(IncomeForDeleteDto incomeForDeleteDto)
         {
-            if (!await userHelper.UserExists(incomeForDeleteDto.Username))
-                return BadRequest("This username doesn't exists");
+            try
+            {
+                if (!await userHelper.UserExists(incomeForDeleteDto.Username))
+                    return BadRequest("This username doesn't exists");
 
-            var deletedIncomes = await repository.DeleteIncomes(incomeForDeleteDto);
+                var deletedIncomes = await repository.DeleteIncomes(incomeForDeleteDto);
 
-            return StatusCode(204);
+                return StatusCode(204);
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
         }
     }
 }

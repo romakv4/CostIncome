d434a230345a2a2b55d756124abfea97c4d31f2b
romakv4 <romakv4@gmail.com>
2019-09-06 00:05:52 +0600

    add first tests

--- CostIncomeCalculator.Tests/Helpers/DatesHelperTests.cs
+++ CostIncomeCalculator.Tests/Helpers/DatesHelperTests.cs
@@ -2,11 +2,25 @@
 using System.Collections.Generic;
 using System.Text;
 using Xunit;
-using CostIncomeCalculator.Helpers.DatesHelper;
+using CostIncomeCalculator.Helpers;
 
 namespace CostIncomeCalculator.Tests.Helpers
 {
-    class DatesHelperTests
+    public class DatesHelperTests
     {
+
+        [Fact]
+        public void GetWeekDateRangeTest()
+        {
+            DatesHelper datesHelper = new DatesHelper();
+            Assert.Equal((new DateTime(2019, 9, 2), new DateTime(2019, 9, 8)), datesHelper.GetWeekDateRange(new DateTime(2019, 9, 5)));
+        }
+
+        [Fact]
+        public void GetMonthDateRangeTest()
+        {
+            DatesHelper datesHelper = new DatesHelper();
+            Assert.Equal((new DateTime(2019, 9, 1), new DateTime(2019, 9, 30)), datesHelper.GetMonthDateRange(new DateTime(2019, 9, 5)));
+        }
     }
 }

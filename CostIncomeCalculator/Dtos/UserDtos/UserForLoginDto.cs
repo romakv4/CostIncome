2918086e69a909dcc6d6305f942c245a6e9caf7b
romakv4 <romakv4@gmail.com>
2019-09-14 22:03:27 +0600

    add docs for user dtos

--- CostIncomeCalculator/Dtos/UserDtos/UserForLoginDto.cs
+++ CostIncomeCalculator/Dtos/UserDtos/UserForLoginDto.cs
@@ -1,8 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace CostIncomeCalculator.Dtos.UserDtos
 {
+    /// <summary>
+    /// Data transfer object for login.
+    /// </summary>
     public class UserForLoginDto
     {
+        /// <summary>
+        /// Username. Required.
+        /// </summary>
+        /// <value>string</value>
+        [Required]
         public string Username { get; set; }
+        /// <summary>
+        /// Password. Required.
+        /// </summary>
+        /// <value>string</value>
+        [Required]
+        [StringLength(30, MinimumLength = 8, ErrorMessage = "You must specify password between 8 and 30 characters")]
         public string Password { get; set; }
     }
 }
--- CostIncomeCalculator/Dtos/UserDtos/UserForRegisterDto.cs
+++ CostIncomeCalculator/Dtos/UserDtos/UserForRegisterDto.cs
@@ -2,11 +2,22 @@ using System.ComponentModel.DataAnnotations;
 
 namespace CostIncomeCalculator.Dtos.UserDtos
 {
+    /// <summary>
+    /// Data transfer object for new user registration.
+    /// </summary>
     public class UserForRegisterDto
     {
+        /// <summary>
+        /// Username. Required.
+        /// </summary>
+        /// <value>string</value>
         [Required]
         public string Username { get; set; }
 
+        /// <summary>
+        /// Password. Required.
+        /// </summary>
+        /// <value>string</value>
         [Required]
         [StringLength(30, MinimumLength = 8, ErrorMessage = "You must specify password between 8 and 30 characters")]
         public string Password { get; set; }

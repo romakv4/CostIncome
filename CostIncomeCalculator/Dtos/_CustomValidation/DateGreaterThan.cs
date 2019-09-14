3fe37c2f3e4afe9b12b6818ffcf629d0c35a4fbd
romakv4 <romakv4@gmail.com>
2019-09-15 01:40:51 +0600

    add docs for custom date validation

--- CostIncomeCalculator/Dtos/_CustomValidation/DateGreaterThan.cs
+++ CostIncomeCalculator/Dtos/_CustomValidation/DateGreaterThan.cs
@@ -4,14 +4,30 @@ using System.Linq;
 
 namespace CostIncomeCalculator.Dtos._CustomValidation
 {
+    /// <summary>
+    /// Date greater than checker class.
+    /// </summary>
     public class DateGreaterThan : ValidationAttribute
     {
         private readonly string startDatePropertyName;
+
+        /// <summary>
+        /// Date greater than constructor.
+        /// Examples of use:
+        /// <see cref="CostIncomeCalculator.Dtos.LimitDtos.LimitForSetDto" />
+        /// </summary>
+        /// <param name="startDatePropertyName">Start date prop name for check.</param>
         public DateGreaterThan(string startDatePropertyName)
         {
             this.startDatePropertyName = startDatePropertyName;
         }
 
+        /// <summary>
+        /// IsValid method override.
+        /// </summary>
+        /// <param name="value">object</param>
+        /// <param name="validationContext">Current validation context</param>
+        /// <returns>If end date greater than start date return false, else true.</returns>
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
             var propertyInfo = validationContext.ObjectType.GetProperty(this.startDatePropertyName);

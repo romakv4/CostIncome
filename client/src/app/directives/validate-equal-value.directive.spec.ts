244159cd0f4b200068577dc0c0defd0d4618c8cc
romakv4 <romakv4@gmail.com>
2020-06-28 14:32:02 +0600

    Drop default test files

--- client/src/app/directives/validate-equal-value.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ValidateEqualValueDirective } from './validate-equal-value.directive';
-
-describe('ValidateEqualValueDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ValidateEqualValueDirective();
-    expect(directive).toBeTruthy();
-  });
-});

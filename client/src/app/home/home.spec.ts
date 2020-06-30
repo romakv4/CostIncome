c08680da0f44e119b1cc6ef93f1130d0f46db210
romakv4 <romakv4@gmail.com>
2020-06-30 17:16:11 +0600

    Add home tests

--- client/src/app/home/home.spec.ts
+++ client/src/app/home/home.spec.ts
@@ -5,6 +5,7 @@ import { GeneralActionsBarComponent } from '../general-actions-bar/general-actio
 import { AccountingItemsTableComponent } from '../accounting-items-table/accounting-items-table.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { Router } from '@angular/router';
+import { By } from '@angular/platform-browser';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -36,5 +37,13 @@ describe('HomeComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have there is nothing', () => {
+    expect(fixture.debugElement.queryAll(By.css('div > div > button')).length).toEqual(2);
+    const addCost = fixture.debugElement.query(By.css('button[data-cy="add-cost"]')).nativeElement;
+    expect(addCost.innerHTML).toBe('Add cost');
+    const addIncome = fixture.debugElement.query(By.css('button[data-cy="add-income"]')).nativeElement;
+    expect(addIncome.innerHTML).toBe('Add income');
   });
 });
